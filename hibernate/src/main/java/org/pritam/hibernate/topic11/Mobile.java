ecc7353691feb8a68a07242553705db5eb1277b6
Pritam Saha <saha.pritam.work@gmail.com>
2023-06-30 17:34:17 +0530

    Topic 11 - c.

--- hibernate/src/main/java/org/pritam/hibernate/topic11/Mobile.java
+++ hibernate/src/main/java/org/pritam/hibernate/topic11/Mobile.java
@@ -3,6 +3,7 @@ package org.pritam.hibernate.topic11;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
@@ -14,6 +15,7 @@ public class Mobile {
 	private String mobileNumber;
 	
 	@ManyToOne
+	@JoinColumn(name="PersonID",referencedColumnName ="PersonID" )
 	private Person person;
 
 	public Mobile() {
