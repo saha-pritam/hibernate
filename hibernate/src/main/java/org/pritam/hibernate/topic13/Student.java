9ced2713ad8a140c8af0c64ebccdfb81ed9631bb
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-14 22:59:38 +0530

    Topic 13 - a.

---
+++ hibernate/src/main/java/org/pritam/hibernate/topic13/Student.java
@@ -0,0 +1,71 @@
+package org.pritam.hibernate.topic13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Students")
+public class Student {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="SId")
+	private int sid;
+	
+	@Column(name="Name")
+	private String name;
+	
+	@ManyToMany
+	private List<Teacher> teachers;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(String name) {
+		super();
+		this.name = name;
+		this.teachers = new ArrayList<Teacher>();
+	}
+
+	public int getId() {
+		return sid;
+	}
+
+	public void setId(int sid) {
+		this.sid = sid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Teacher> getTeachers() {
+		return teachers;
+	}
+
+	public void setTeachers(List<Teacher> teachers) {
+		this.teachers = teachers;
+	}
+
+	@Override
+	public String toString() {
+		String s = "Student [id=" + sid + ", name=" + name + ",\n Teachers :- {";
+		for(Teacher teacher:teachers)
+			s = s + "\n" + teacher.getName();
+			s = s + "\n}]";
+		return s;
+	}
+}
---
+++ hibernate/src/main/java/org/pritam/hibernate/topic13/Teacher.java
@@ -0,0 +1,72 @@
+package org.pritam.hibernate.topic13;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Teachers")
+public class Teacher {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="TId")
+	private int tid;
+	
+	@Column(name="Name")
+	private String name;
+	
+	@ManyToMany
+	private List<Student> students;
+
+	public Teacher() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Teacher(String name) {
+		super();
+		this.name = name;
+		this.students = new ArrayList<Student>();
+	}
+
+	public int getId() {
+		return tid;
+	}
+
+	public void setId(int tid) {
+		this.tid = tid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	@Override
+	public String toString() {
+		String s =  "Teacher [id=" + tid + ", name=" + name + ",\nStudents :- {";
+		for(Student student:students)
+			s = s + "\n" + student.getName();
+		s = s + "\n}]";
+		return s;
+	}
+}
