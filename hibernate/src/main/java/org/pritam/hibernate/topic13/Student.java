be96cd0608839d1017d46b8cde30acb95434a2e8
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-14 23:12:57 +0530

    Topic 13 - e.

--- hibernate/src/main/java/org/pritam/hibernate/topic13/Student.java
+++ hibernate/src/main/java/org/pritam/hibernate/topic13/Student.java
@@ -8,8 +8,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
@@ -24,8 +22,7 @@ public class Student {
 	@Column(name="Name")
 	private String name;
 	
-	@ManyToMany
-	@JoinTable(name="StudentTeacher",joinColumns = @JoinColumn(name="SID",referencedColumnName = "sid"),inverseJoinColumns = @JoinColumn(name="TID",referencedColumnName = "tid"))
+	@ManyToMany(mappedBy="students")
 	private List<Teacher> teachers;
 	
 	public Student() {
