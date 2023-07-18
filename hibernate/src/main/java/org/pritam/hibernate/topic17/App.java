6a81001b10656c4103047059f0277a1985203ca7
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-18 22:29:17 +0530

    Topic 17 - p.

--- hibernate/src/main/java/org/pritam/hibernate/topic17/App.java
+++ hibernate/src/main/java/org/pritam/hibernate/topic17/App.java
@@ -6,9 +6,11 @@ import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaCriteriaUpdate;
 import org.hibernate.query.criteria.JpaRoot;
 
 import jakarta.persistence.criteria.Expression;
+import jakarta.persistence.criteria.Root;
 import jakarta.persistence.criteria.Selection;
 
 public class App {
@@ -17,14 +19,16 @@ public class App {
 		SessionFactory sessionFactory = new Configuration().configure("org/pritam/hibernate/topic17/hibernate.cfg.xml").buildSessionFactory();
 		Session session = sessionFactory.openSession();
 		
-		HibernateCriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-		JpaCriteriaQuery<Double> criteriaQuery = criteriaBuilder.createQuery(Double.class);
-		JpaRoot<Student> root = criteriaQuery.from(Student.class);
-			
-		criteriaQuery.select((Selection)criteriaBuilder.avg((Expression)root.get("mark")));
-		Query<Double> query = session.createQuery(criteriaQuery);
-		System.out.println("Avg of all marks :- "+query.uniqueResult());
-			
+		HibernateCriteriaBuilder builder = session.getCriteriaBuilder();
+		JpaCriteriaUpdate<Student> criteriaUpdateQuery = builder.createCriteriaUpdate(Student.class);
+		Root<Student> root = criteriaUpdateQuery.from(Student.class);
+		criteriaUpdateQuery.set("name", "Pritam Saha");
+		criteriaUpdateQuery.where(builder.equal(root.get("id"), 2));
+		
+		session.beginTransaction();
+		session.createQuery(criteriaUpdateQuery).executeUpdate();
+		session.getTransaction().commit();
+		
 		session.close();
 		sessionFactory.close();
 	}
