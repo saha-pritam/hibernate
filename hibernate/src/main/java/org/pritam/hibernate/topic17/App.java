75808cf12a64329819d65227f4eaedf3511c19d9
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-18 20:36:48 +0530

    Topic 17 - c.

--- hibernate/src/main/java/org/pritam/hibernate/topic17/App.java
+++ hibernate/src/main/java/org/pritam/hibernate/topic17/App.java
@@ -3,6 +3,7 @@ package org.pritam.hibernate.topic17;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
 import org.hibernate.query.criteria.JpaCriteriaQuery;
 import org.hibernate.query.criteria.JpaRoot;
@@ -17,6 +18,11 @@ public class App {
 		HibernateCriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
 		JpaCriteriaQuery<Student> criteriaQuery = criteriaBuilder.createQuery(Student.class);
 		JpaRoot<Student> root = criteriaQuery.from(Student.class);
+		
+		//Select all using criteria API
+		criteriaQuery.select(root);
+		Query<Student> query = session.createQuery(criteriaQuery);
+		System.out.println(query.list());
 		
 		session.close();
 		sessionFactory.close();
