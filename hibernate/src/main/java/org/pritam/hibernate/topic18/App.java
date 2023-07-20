bfc488014c9172afad5dab41e2e2d9af33d9adff
Pritam Saha <saha.pritam.work@gmail.com>
2023-07-20 13:02:56 +0530

    Topic 18 - e.

---
+++ hibernate/src/main/java/org/pritam/hibernate/topic18/App.java
@@ -0,0 +1,27 @@
+package org.pritam.hibernate.topic18;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class App {
+
+	public static void main(String[] args) {
+		SessionFactory sessionFactory = new Configuration().configure("org/pritam/hibernate/topic18/hibernate.cfg.xml").buildSessionFactory();
+		Session session1 = sessionFactory.openSession();
+		Student s = session1.get(Student.class, 1);
+		System.out.println("Student 1 details :- "+s);
+		System.out.println("Again trying to fetch student 1 details");
+		s = session1.get(Student.class, 1); //No query to DB will be passed
+		System.out.println("Student 1 details :- "+s);
+		session1.close();
+		
+		
+		Session session2 = sessionFactory.openSession();
+		System.out.println("Again trying to fetch student 1 details after closing previous session");
+		s = session2.get(Student.class, 1);
+		System.out.println("Student 1 details :- "+s);//Query to DB will be passed
+		session2.close();
+		sessionFactory.close();
+	}
+}
---
+++ hibernate/src/main/java/org/pritam/hibernate/topic18/Student.java
@@ -0,0 +1,79 @@
+package org.pritam.hibernate.topic18;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Students")
+public class Student {
+	@Id
+	@Column(name="Id")
+	private int sid;
+	
+	@Column(name="Name")
+	private String name;
+	
+	@Column(name="Class")
+	private String std;
+	
+	@Column(name="Mark")
+	private int mark;
+	
+	@Column(name="Gender")
+	private String gender;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getStd() {
+		return std;
+	}
+
+	public void setStd(String std) {
+		this.std = std;
+	}
+
+	public int getMark() {
+		return mark;
+	}
+
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", name=" + name + ", std=" + std + ", mark=" + mark + ", gender=" + gender
+				+ "]";
+	}
+}
